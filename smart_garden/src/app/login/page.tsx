a209bcd5b29e65bd12f363eb85b52c3803c51c73
Damal Ranula <106365502+DamalP@users.noreply.github.com>
2024-07-20 14:43:36 +0530

    profile structure and login structure is done

--- smart_garden/src/app/login/page.tsx
+++ smart_garden/src/app/login/page.tsx
@@ -1,7 +1,50 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import {useRouter} from "next/navigation";
+import {axios} from "axios";
+
 export default function LoginPage() {
+
+    const [user, setUser] = React.useState({
+        email: "",
+        password: "",
+    });
+
+    const onLogin = async () => {
+
+    }
+
     return (
-        <div className="flex">
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1 className="text-center text-white text-2xl">Login</h1>
+            <hr/>
+            <label htmlFor="email">email</label>
+            <input
+                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+                type="email"
+                id="email"
+                value={user.email}
+                onChange={(e) => setUser({...user, email: e.target.value})}
+                placeholder="email"
+            />
+            <label htmlFor="password">password</label>
+            <input
+                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+                type="password"
+                id="password"
+                value={user.password}
+                onChange={(e) =>
+                    setUser({...user, password: e.target.value})}
+                placeholder="password"
+            />
+            <button
+                className="p-2 border border-gray-300 rounde-lg mb-4 focus:outline-none
+                focus:border-gray-600"
+                onClick={onLogin}
+            > Login Here </button>
+
+            <Link href="/signup">Visit Signup Page</Link>
         </div>
     )
 }
